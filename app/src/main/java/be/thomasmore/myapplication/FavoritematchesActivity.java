36a44245156664fe08bba7b76a0f8f726a2d4d20
Julian <r0697475@student.thomasmore.be>
2019-11-14 14:07:19 +0100

    Navigation done

--- app/src/main/java/be/thomasmore/myapplication/FavoritematchesActivity.java
+++ app/src/main/java/be/thomasmore/myapplication/FavoritematchesActivity.java
@@ -3,59 +3,45 @@ package be.thomasmore.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class FavoritematchesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favoritematches);
 
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-
-        Button btn =(Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MatchesActivity.class));
-            }
-        });
     }
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
 
                     switch (item.getItemId()){
                         case R.id.nav_home:
-                            Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
+                            Intent intent0 = new Intent(FavoritematchesActivity.this, MainActivity.class);
+                            startActivity(intent0);
                             break;
                         case R.id.nav_favoritematches:
-                            Intent intent1 = new Intent(MainActivity.this, FavoritematchesActivity.class);
-                            startActivity(intent1);
+//                            Intent intent1 = new Intent(FavoritematchesActivity.this, FavoriteteamsActivity.class);
+//                            startActivity(intent1);
 
                             break;
                         case R.id.nav_favoriteteams:
-                            Intent intent2 = new Intent(MainActivity.this, FavoriteteamsActivity.class);
+                            Intent intent2 = new Intent(FavoritematchesActivity.this, FavoriteteamsActivity.class);
                             startActivity(intent2);
                             break;
                         case R.id.nav_settings:
-                            Intent intent3 = new Intent(MainActivity.this, SettingsActivity.class);
+                            Intent intent3 = new Intent(FavoritematchesActivity.this, SettingsActivity.class);
                             startActivity(intent3);
                             break;
                     }
--- app/src/main/java/be/thomasmore/myapplication/FavoriteteamsActivity.java
+++ app/src/main/java/be/thomasmore/myapplication/FavoriteteamsActivity.java
@@ -3,59 +3,45 @@ package be.thomasmore.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class FavoriteteamsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favoriteteams);
 
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-
-        Button btn =(Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MatchesActivity.class));
-            }
-        });
     }
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
 
                     switch (item.getItemId()){
                         case R.id.nav_home:
-                            Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
+                            Intent intent0 = new Intent(FavoriteteamsActivity.this, MainActivity.class);
+                            startActivity(intent0);
                             break;
                         case R.id.nav_favoritematches:
-                            Intent intent1 = new Intent(MainActivity.this, FavoritematchesActivity.class);
+                            Intent intent1 = new Intent(FavoriteteamsActivity.this, FavoritematchesActivity.class);
                             startActivity(intent1);
 
                             break;
                         case R.id.nav_favoriteteams:
-                            Intent intent2 = new Intent(MainActivity.this, FavoriteteamsActivity.class);
-                            startActivity(intent2);
+//                            Intent intent2 = new Intent(FavoriteteamsActivity.this, FavoriteteamsActivity.class);
+//                            startActivity(intent2);
                             break;
                         case R.id.nav_settings:
-                            Intent intent3 = new Intent(MainActivity.this, SettingsActivity.class);
+                            Intent intent3 = new Intent(FavoriteteamsActivity.this, SettingsActivity.class);
                             startActivity(intent3);
                             break;
                     }
