5776864752ce8f40bff21fd5ea6e9bcee6b47a3e
Tomas Marlein <tomas.marlein@gmail.com>
2019-12-20 19:14:56 +0100

    Layout Add wedstrijd pagina

--- app/src/main/java/be/thomasmore/voetbalapp2/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/be/thomasmore/voetbalapp2/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -15,12 +18,20 @@ import be.thomasmore.voetbalapp2.R;
 public class NotificationsFragment extends Fragment {
 
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+
+
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         return root;
+
+
     }
+
+
 
     @Override
     public void onResume() {
